41d6030a05ba445d612fcec5d6d054aab004b73c
uvsq22203427 <114875074+uvsq22203427@users.noreply.github.com>
2023-03-24 11:25:43 +0100

    Update 2048.py

--- 2048.py
+++ 2048.py
@@ -19,3 +19,38 @@ for i in range(8):
 
 racine.title("2048")
 racine.mainloop() # Lancement de la boucle principale
+
+#boutons à finaliser
+
+racine = tk.Tk
+racine.title("2048")
+texte = tk.Label(text= "Commencer à jouer")
+text.grid(column= , row = )
+b1 = tk.Button(text = "Play", command = #mettre 2 tuiles de manière aléatoire (2 ou 4) dans la grilles)
+b1.grid( column =  , row = )
+racine.mainloop()
+
+import tkinter as tk
+racine = tk.Tk
+racine.title("2048")
+texte = tk.Label(text= "Terminer la partie et voir le score")
+text.grid(column= , row = )
+b1 = tk.Button(text = "Exit", command = #finir la partie et afficher le score)
+b1.grid( column =  , row = )
+racine.mainloop() 
+
+import tkinter as tk
+racine = tk.Tk
+racine.title("2048")
+texte = tk.Label(text= "Sauvergarder la partie")
+text.grid(column= , row = )
+b1 = tk.Button(text = "Save", command = #sauvergarder une partie en cours dans un fichier)
+b1.grid( column =  , row = )
+
+import tkinter as tk
+racine = tk.Tk
+racine.title("2048")
+texte = tk.Label(text= "Reprendre une partie")
+text.grid(column= , row = )
+b1 = tk.Button(text = "Resume the game", command = #reprendre une partie enregistrée)
+b1.grid( column =  , row = )
