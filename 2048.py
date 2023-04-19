a86c08883ba1a0046423016675d349288ea045fd
Heddie BEN MARZOUK <heddie.benmarzouk.uvsq.fr>
2023-04-19 16:20:47 +0200

    Nouvelle grille

--- 2048.py
+++ 2048.py
@@ -1,13 +1,6 @@
 import tkinter as tk
 import random as rd
 
-gamebox=[[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-tuiles= [2,2,2,2,2,2,2,2,2,4]
-
-
-HEIGHT = 320
-WIDTH = 320
-score = 0
 
 def choisir_tuiles_au_hasard():
     tuiles_au_hasard=[]
@@ -17,18 +10,22 @@ def choisir_tuiles_au_hasard():
         tuiles_au_hasard=tuiles_au_hasard+tuiles[AP_tuiles]
     return tuiles_au_hasard
 
-a = choisir_tuiles_au_hasard()
-
 
 
 def déplacement_droite():
+    pass
+
 
 def déplacement_gauche():
+    pass
+
 
 def déplacement_haut():
+    pass
+
 
 def déplacement_bas():
-
+    pass
 
 
 def sauvergarder_score():
@@ -37,57 +34,22 @@ def sauvergarder_score():
     mon_fichier.close()
    
 def play():
+    pass
     
     
 
-racine = tk.Tk() # Création de la fenêtre racinejjjjjjjj
-canvas = tk.Canvas(racine, bg="PaleGreen1", height=HEIGHT, width=WIDTH)
-canvas.grid()
+racine = tk.Tk()
+racine.title("2048")
 
-def dessiner_lignes():
-    #lignes horizontales
-    canvas.create_line(1, 3, 320, 3,fill='black', width=10)
-    canvas.create_line(1, 80, 320, 80,fill='black', width=3)
-    canvas.create_line(1, 160, 320, 160,fill='black', width=3)
-    canvas.create_line(1, 240, 320, 240,fill='black', width=3)
-    canvas.create_line(1, 320, 320, 320, fill='black', width=10)
- 
-    #lignes verticales
-    canvas.create_line(3, 1, 3, 320,fill='black', width=10)
-    canvas.create_line(80, 1, 80, 320,fill='black', width=3)
-    canvas.create_line(160, 1, 160, 320,fill='black', width=3)
-    canvas.create_line(240, 1, 240, 320,fill='black', width=3)
-    canvas.create_line(320, 1, 320, 325, fill='black', width=10)
-
-def dessiner_chiffres(tab):
-    k=0
-    l=0
-    for i in range(0,4):
-        if i==0:
-            k+=40
-        else:
-            k+=80
-        l=0
-        for j in range(0,4):
-            if j==0:
-                l+=40
-            else:
-                l+=80
-            if tab[i][j]!=0:
-                canvas.create_text(l, k, text=str(tab[i][j]), font=("arial",30,"bold"))
-
-def actualiser_fenetre(tab):
-    canvas.delete(ALL)
-    dessiner_lignes()
-    dessiner_chiffres(tab)
- 
-console_affiche(gamebox)
- 
-actualiser_fenetre(gamebox) # check
- 
-canvas.focus_set()
-canvas.bind('<Key>', gestion_clavier)
-canvas.pack(padx=5, pady=5)
+# Créer une grille
+grid = []
+for i in range(4):
+    row = []
+    for j in range(4):
+        label = tk.Label(racine, text="", width=4, height=2, font=("Arial", 20, "bold"), bg="gray80")
+        label.grid(row=i, column=j, padx=5, pady=5)
+        row.append(label)
+    grid.append(row)
         
 
 texte = tk.Label(text= "Commencer à jouer")
@@ -134,3 +96,4 @@ b1 = tk.Button(text = "Bas", command = déplacement_bas , font = ("helvetica", "
 b1.grid( column =  0, row = 1)
 
 racine.mainloop()
+
