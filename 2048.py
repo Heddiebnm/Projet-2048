b980f4e711ff8498f5dd5373a5af8d09113ddad9
Heddie BEN MARZOUK <heddie.benmarzouk.uvsq.fr>
2023-04-25 17:22:16 +0200

    Modif.25/04

--- 2048.py
+++ 2048.py
@@ -1,153 +1,104 @@
 import tkinter as tk
-import random as rd
+import random 
+
+# Créer une fenêtre
+racine = tk.Tk()
+racine.title("2048")
+
+# Créer une grille
+grid = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+
+for i in range(4):
+    for j in range(4):
+        label = tk.Label(racine, text=grid[i][j], width=12, height=6, font=("Arial", 20, "bold"), bg="gray80")
+        label.grid(row=i, column=j, padx=5, pady=5)
 
 
-def choisir_tuiles_au_hasard():
-    tuiles_au_hasard=[]
-    for i in range (1):
-        tuiles= [2,2,2,2,2,2,2,2,2,4]
-        AP_tuiles = rd.randint(0,len(tuiles)-1)
-        tuiles_au_hasard=tuiles_au_hasard+tuiles[AP_tuiles]
-    return tuiles_au_hasard
+def add_tile():
+    global grid
+    empty_cells = [(i, j) for i in range(4) for j in range(4) if grid[i][j] == 0]
+    if empty_cells:
+        i, j = random.choice(empty_cells)
+        grid[i][j] = 2 if random.random() < 0.9 else 4
 
 
-  # Fonction pour déplacer les tuiles vers la droite.
-def déplacement_droite():
+
+
+def start():
+    add_tile()
+    add_tile()
     for i in range(4):
-        for j in range(3, 0, -1):
-            if grid[i][j] == 0:
-                for k in range(j-1, -1, -1):
-                    if grid[i][k] != 0:
-                        grid[i][j], grid[i][k] = grid[i][k], grid[i][j]
-                        break
+        for j in range(4):
+            label = racine.grid_slaves(row=i, column=j)[0]
+            label["text"] =str(grid[i][j])
+    
 
-        # Fusion des tuiles identiques.
+def déplacement_droite():# Déplacement des tuiles.
+    for i in range(4):# Fusion des tuiles identiques.
         for j in range(3, 0, -1):
             if grid[i][j] != 0 and grid[i][j] == grid[i][j-1]:
                 grid[i][j] *= 2
                 grid[i][j-1] = 0
 
-
-# Fonction pour déplacer les tuiles vers la gauche.
 def déplacement_gauche():
     for i in range(4):
-                for j in range(3):
-            if grid[i][j] == 0:
-                for k in range(j+1, 4):
-                    if grid[i][k] != 0:
-                        grid[i][j], grid[i][k] = grid[i][k], grid[i][j]
-                        break
-
         # Fusion des tuiles identiques.
         for j in range(3):
             if grid[i][j] != 0 and grid[i][j] == grid[i][j+1]:
                 grid[i][j] *= 2
                 grid[i][j+1] = 0
 
+        # Déplacement des tuiles.
+        for j in range(3):
+            if grid[i][j] == 0:
+                for k in range(j+1, 4):
+                    if grid[i][k] != 0:
+                        grid[i][j], grid[i][k] = grid[i][k], grid[i][j]
+                        break
 
-
-# Fonction pour déplacer les tuiles vers le haut.
 def déplacement_haut():
-    for j in range(4):
-        for i in range(3):
-            if grid[i][j] == 0:
-                for k in range(i+1, 4):
-                    if grid[k][j] != 0:
-                        grid[i][j], grid[k][j] = grid[k][j], grid[i][j]
-                        break
-
-        # Fusion des tuiles identiques.
-        for i in range(3):
-            if grid[i][j] != 0 and grid[i][j] == grid[i+1][j]:
-                grid[i][j] *= 2
-                grid[i+1][j] = 0
-
-
-# Fonction pour déplacer les tuiles vers le bas.
-def déplacement_bas():
-    for j in range(4):
-        for i in range(3, 0, -1):
-            if grid[i][j] == 0:
-                for k in range(i-1, -1, -1):
-                    if grid[k][j] != 0:
-                        grid[i][j], grid[k][j] = grid[k][j], grid[i][j]
-                        break
-
-        # Fusion des tuiles identiques.
-        for i in range(3, 0, -1):
-            if grid[i][j] != 0 and grid[i][j] == grid[i-1][j]:
-                grid[i][j] *= 2
-                grid[i-1][j] = 0
-       
-
-
-
-def sauvergarder_score():
-    mon_fichier = open("fichier.txt", "w")
-    mon_fichier.write("user01 :", score)
-    mon_fichier.close()
-   
-def play():
     pass
-    
-    
 
-racine = tk.Tk()
-racine.title("2048")
+def déplacement_bas():
+    pass
 
-# Créer une grille
-grid = []
-for i in range(4):
-    row = []
-    for j in range(4):
-        label = tk.Label(racine, text="", width=4, height=2, font=("Arial", 20, "bold"), bg="gray80")
-        label.grid(row=i, column=j, padx=5, pady=5)
-        row.append(label)
-    grid.append(row)
-        
+def reset():
+    pass
 
-texte = tk.Label(text= "Commencer à jouer")
-texte.grid(column= 0, row = 1)
-b1 = tk.Button(text = "Play", command = play , font = ("helvetica", "26")) #mettre 2 tuiles de manière aléatoire (2 ou 4) dans la grilles
-b1.grid( column =  0, row = 1)
+def save():
+    pass
+
+def start():
+    pass
+
+def end():
+    pass
+
+start()
+
+Button1 = tk.Button(racine, text="Droite", command= déplacement_droite , font=("helvetica", "20"), relief="groove")
+Button1.grid(column=6, row=2) # positionnement du bouton de déplacement vers la droite
+
+Button2 = tk.Button(racine, text="Gauche", command= déplacement_gauche , font=("helvetica", "20"), relief="groove")
+Button2.grid(column=4, row=2) # positionnement du bouton de déplacement vers la gauche
+
+Button3 = tk.Button(racine, text="Haut", command= déplacement_haut , font=("helvetica", "20"), relief="groove")
+Button3.grid(column=5, row=1) # positionnement du bouton de déplacement vers le haut 
+
+Button4 = tk.Button(racine, text="Bas", command = déplacement_bas , font = ("helvetica", "20"), relief = "groove") # création d'un widget
+Button4.grid(column=5, row=3) # positionnement du bouton dedéplacement vers le bas 
+
+Button5 = tk.Button(racine, text="Reset", command = reset , font = ("helvetica", "20"), relief = "groove") # création d'un widget
+Button5.grid(column=4, row=0) # positionnement du bouton reset
+
+Button6 = tk.Button(racine, text="Save", command = save , font = ("helvetica", "20"), relief = "groove") # création d'un widget
+Button6.grid(column=6, row=0) # positionnement du bouton save
+
+button7 = tk.Button(racine, text="Start", command = start , font = ("helvetica", "20"), relief = "groove") # création d'un widget
+button7.grid(column=4, row=4) # positionnement du bouton start
+
+Button8 = tk.Button(racine, text="End", command = end , font = ("helvetica", "20"), relief = "groove") # création d'un widget
+Button8.grid(column=6, row=4) # positionnement du bouton end
 
 
-texte = tk.Label(text= "Terminer la partie et voir le score")
-texte.grid(column= 1, row = 1)
-b1 = tk.Button(text = "Exit", command = "" , font = ("helvetica", "26") ) #finir la partie et afficher le score)
-b1.grid( column =  1, row = 1)
-
-
-texte = tk.Label(text= "Sauvergarder la partie")
-texte.grid(column= 0, row = 1)
-b1 = tk.Button(text = "Save", command = sauvergarder_score, font = ("helvetica", "26"))#sauvergarder une partie en cours dans un fichier)
-b1.grid( column =  0, row = 1)
-               
-
-texte = tk.Label(text= "Reprendre une partie")
-texte.grid(column= 1, row = 2)
-b1 = tk.Button(text = "Resume the game", command = "c", font = ("helvetica", "26"))#reprendre une partie enregistrée)
-b1.grid( column =  1, row = 2)
-
-texte = tk.Label(text= "se déplacer à droite")
-texte.grid(column= 0, row = 1)
-b1 = tk.Button(text = "Droite", command = déplacement_droite , font = ("helvetica", "26")) #boutton permettant le déplacement à droite
-b1.grid( column =  0, row = 1)
-
-texte = tk.Label(text= "se déplacer à gauche")
-texte.grid(column= 0, row = 1)
-b1 = tk.Button(text = "Gauche", command = déplacement_gauche , font = ("helvetica", "26")) #boutton permettant le déplacement à gauche
-b1.grid( column =  0, row = 1)
-
-texte = tk.Label(text= "se déplacer en haut")
-texte.grid(column= 0, row = 1)
-b1 = tk.Button(text = "Haut", command = déplacement_haut , font = ("helvetica", "26")) #boutton permettant le déplacement en haut
-b1.grid( column =  0, row = 1)
-
-texte = tk.Label(text= "se déplacer en bas")
-texte.grid(column= 0, row = 1)
-b1 = tk.Button(text = "Bas", command = déplacement_bas , font = ("helvetica", "26")) #boutton permettant le déplacement en bas 
-b1.grid( column =  0, row = 1)
-
-racine.mainloop()
-
+racine.mainloop() # permet d'afficher la fenêtre 
